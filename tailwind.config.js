7aa6fb74d92cf4da335c4e0ca945a51aefdc15d6
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2025-01-11 16:38:43 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
+
      "./src/**/*.{js,jsx,ts,tsx}"
-=======
-    "./src/**/*.{js,jsx,ts,tsx}",
->>>>>>> 3f480ae76cb88cf2b1bb45410de5d8ffc4416d04
+
+  
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
+
 }
 
-=======
+
 }
->>>>>>> 3f480ae76cb88cf2b1bb45410de5d8ffc4416d04
+
